6406925154e13b44e337257d25c7941108a07b2e
furkanndev <furkanfr.gltkn@gmail.com>
2023-01-08 14:45:41 +0300

    resimler eklendi, giriş text'i yazıldı

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const images = [...document.querySelectorAll('.image')];
+
+
+
+const popup = document.querySelector('.popup');
+const largeImage = document.querySelector('.large-image');
+const closeBtn = document.querySelector('.close-btn');
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `img/img${i+1}.jpg`;
+    largeImage.src = path;
+}
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+})
